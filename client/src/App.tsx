71e5008030ff2dfd7dad849377e991b5218a4a59
Alex Mickelson <alexmickelson96@gmail.com>
2026-02-13 14:43:16 -0700

    frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router";
 import { UnitListPage } from "./features/UnitListPage";
-
 function App() {
   return (
     <>
@@ -9,6 +8,18 @@ function App() {
       <Routes>
         <Route path="/" element={<UnitListPage />} />
       </Routes>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+              <h2>AI Output</h2>
+              <ul>
+                <li>Unit 1: Marine</li>
+                <li>Unit 2: Zergling</li>
+                <li>Unit 3: Zealot</li>
+              </ul>
+          `,
+        }}
+      ></div>
     </>
   );
 }
